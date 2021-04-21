571cd503340d92f77099d9f45c51e4e8545504a2
Yap Ying Ying <yapyingying94@gmail.com>
2021-04-21 22:17:29 +0800

    added results parsing

--- src/visualization.py
+++ src/visualization.py
@@ -1,6 +1,42 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import pandas as pd
+from sklearn.metrics import confusion_matrix
+
+
+def get_prec_at_k(k, df):
+    return df.head(k).true_labels.sum() / k
+
+
+def parse_results(df_probability, df_true_labels):
+    '''
+    This function will print out all the results for the project
+    Arguments
+    ---------
+    df_probability: probability matrix. Has two headers: proba. proba is the probability that the model predicted 1 
+                    { e.g. model.predict_proba(df_test, axis=1))[:, 1] }
+    df_true_labels: true label matrix. Has two headers: name and true_labels.
+    '''
+    df_combine = df_probability.merge(df_true_labels, on="name")
+    df_combine.sort_values('proba', ascending=False, inplace=True)
+    y_pred = (df_combine.proba > 0.5).astype(int)
+
+    # Print confusion matrix
+    cf = confusion_matrix(df_combine.true_labels, y_pred)
+    make_confusion_matrix(cf)
+
+    plt.show()
+    print()
+
+    # Printing precision @ k
+    prec_at_5 = get_prec_at_k(5, df_combine)
+    prec_at_10 = get_prec_at_k(10, df_combine)
+    prec_at_15 = get_prec_at_k(15, df_combine)
+    print(f"Precision @ 5: {prec_at_5}")
+    print(f"Precision @ 10: {prec_at_10}")
+    print(f"Precision @ 15: {prec_at_15}")
+
 
 def make_confusion_matrix(cf,
                           group_names=None,
@@ -31,15 +67,14 @@ def make_confusion_matrix(cf,
     figsize:       Tuple representing the figure size. Default will be the matplotlib rcParams value.
     cmap:          Colormap of the values displayed from matplotlib.pyplot.cm. Default is 'Blues'
                    See http://matplotlib.org/examples/color/colormaps_reference.html
-                   
+
     title:         Title for the heatmap. Default is None.
     '''
-
 
     # CODE TO GENERATE TEXT INSIDE EACH SQUARE
     blanks = ['' for i in range(cf.size)]
 
-    if group_names and len(group_names)==cf.size:
+    if group_names and len(group_names) == cf.size:
         group_labels = ["{}\n".format(value) for value in group_names]
     else:
         group_labels = blanks
@@ -54,48 +89,45 @@ def make_confusion_matrix(cf,
     else:
         group_percentages = blanks
 
-    box_labels = [f"{v1}{v2}{v3}".strip() for v1, v2, v3 in zip(group_labels,group_counts,group_percentages)]
-    box_labels = np.asarray(box_labels).reshape(cf.shape[0],cf.shape[1])
-
+    box_labels = [f"{v1}{v2}{v3}".strip() for v1, v2, v3 in zip(group_labels, group_counts, group_percentages)]
+    box_labels = np.asarray(box_labels).reshape(cf.shape[0], cf.shape[1])
 
     # CODE TO GENERATE SUMMARY STATISTICS & TEXT FOR SUMMARY STATS
     if sum_stats:
-        #Accuracy is sum of diagonal divided by total observations
-        accuracy  = np.trace(cf) / float(np.sum(cf))
+        # Accuracy is sum of diagonal divided by total observations
+        accuracy = np.trace(cf) / float(np.sum(cf))
 
-        #if it is a binary confusion matrix, show some more stats
-        if len(cf)==2:
-            #Metrics for Binary Confusion Matrices
-            precision = cf[1,1] / sum(cf[:,1])
-            recall    = cf[1,1] / sum(cf[1,:])
-            f1_score  = 2*precision*recall / (precision + recall)
+        # if it is a binary confusion matrix, show some more stats
+        if len(cf) == 2:
+            # Metrics for Binary Confusion Matrices
+            precision = cf[1, 1] / sum(cf[:, 1])
+            recall = cf[1, 1] / sum(cf[1, :])
+            f1_score = 2*precision*recall / (precision + recall)
             stats_text = "\n\nAccuracy={:0.3f}\nPrecision={:0.3f}\nRecall={:0.3f}\nF1 Score={:0.3f}".format(
-                accuracy,precision,recall,f1_score)
+                accuracy, precision, recall, f1_score)
         else:
             stats_text = "\n\nAccuracy={:0.3f}".format(accuracy)
     else:
         stats_text = ""
 
-
     # SET FIGURE PARAMETERS ACCORDING TO OTHER ARGUMENTS
-    if figsize==None:
-        #Get default figure size if not set
+    if figsize == None:
+        # Get default figure size if not set
         figsize = plt.rcParams.get('figure.figsize')
 
-    if xyticks==False:
-        #Do not show categories if xyticks is False
-        categories=False
-
+    if xyticks == False:
+        # Do not show categories if xyticks is False
+        categories = False
 
     # MAKE THE HEATMAP VISUALIZATION
     plt.figure(figsize=figsize)
-    sns.heatmap(cf,annot=box_labels,fmt="",cmap=cmap,cbar=cbar,xticklabels=categories,yticklabels=categories)
+    sns.heatmap(cf, annot=box_labels, fmt="", cmap=cmap, cbar=cbar, xticklabels=categories, yticklabels=categories)
 
     if xyplotlabels:
         plt.ylabel('True label')
         plt.xlabel('Predicted label' + stats_text)
     else:
         plt.xlabel(stats_text)
-    
+
     if title:
         plt.title(title)
